1efeb9b549811f42f3140f1830c6a793cefdf49b
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-07-18 15:56:11 +0700

    Init project

---
+++ Apple Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WatchListViewController.swift
+//  Apple Stocks
+//
+//  Created by TheGIZzz on 18/7/2565 BE.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
