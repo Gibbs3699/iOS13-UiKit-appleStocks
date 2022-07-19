7a1ac00d8dae21701ceda2974056237785a2d2a5
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-07-19 17:20:48 +0700

    feat: setup searchBar and title

--- Apple Stocks/Controllers/WatchListViewController.swift
+++ Apple Stocks/Controllers/WatchListViewController.swift
@@ -13,8 +13,38 @@ class WatchListViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
+        
+        setupSearchController()
+        setupTitle()
     }
 
 
+    private func setupTitle() {
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.width-20 , height: titleView.height))
+        
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 40, weight: .medium)
+        titleView.addSubview(label)
+        
+        navigationItem.titleView = titleView
+    }
+    
+    private func setupSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+            let resultVC = searchController.searchResultsController as? SearchResultsViewController, !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+    }
+}
---
+++ Apple Stocks/Other/Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Extension.swift
+//  Apple Stocks
+//
+//  Created by TheGIZzz on 19/7/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Framing
+
+extension UIView {
+    /// Width of view
+    var width: CGFloat {
+        frame.size.width
+    }
+
+    /// Height of view
+    var height: CGFloat {
+        frame.size.height
+    }
+
+    /// Left edge of view
+    var left: CGFloat {
+        frame.origin.x
+    }
+
+    /// Right edge of view
+    var right: CGFloat {
+        left + width
+    }
+
+    /// Top edge of view
+    var top: CGFloat {
+        frame.origin.y
+    }
+
+    /// Bottom edge of view
+    var bottom: CGFloat {
+        top + height
+    }
+}
+
